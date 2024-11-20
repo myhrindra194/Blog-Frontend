3b66b8cc3930288ec4a9aa73b2a85739faf27690
myhrindra194 <myhrindra194@gmail.com>
2024-11-20 22:46:51 +0300

    Adjust seconds in dateDiff

--- src/utils/function.js
+++ src/utils/function.js
@@ -12,12 +12,16 @@ export function dateDiff(dateStr) {
     const jours = Math.floor(diff / (1000 * 60 * 60 * 24));
     const heures = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const secondes = Math.floor((diff % (1000 * 60)) / 1000);  
   
     if (jours > 0) {
       return `Il y a ${jours} jour${jours > 1 ? 's' : ''}`;
     } else if (heures > 0) {
       return `Il y a ${heures} heure${heures > 1 ? 's' : ''}`;
-    } else {
+    } else if(minutes > 0){
       return `Il y a ${minutes} minute${minutes > 1 ? 's' : ''}`;
+    }
+    else {
+        return `Il y a ${secondes} seconds`;
     }
   }
